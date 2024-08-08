7ff0da0f74e93c0af97feda33f2c7ffcdd4649ce
Milton Muñoz <livemilton@gmail.com>
2024-08-07 22:45:47 -0500

    first commit

---
+++ src/test/java/com/livemilton/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livemilton.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
