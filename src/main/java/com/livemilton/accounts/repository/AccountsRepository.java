57d87f7433f92cbebd26576a0be17fca34c5537f
Milton Muñoz <livemilton@gmail.com>
2024-08-08 20:02:35 -0500

    Delete API inside accounts microservice

--- src/main/java/com/livemilton/accounts/repository/AccountsRepository.java
+++ src/main/java/com/livemilton/accounts/repository/AccountsRepository.java
@@ -1,11 +1,16 @@
 package com.livemilton.accounts.repository;
 
 import com.livemilton.accounts.entity.Accounts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
     Optional<Accounts> findByCustomerId(Long CustomerId);
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long CustomerId);
 }
